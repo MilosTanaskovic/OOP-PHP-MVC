50f583d7a40de121b4a607f5aceddd288f448341
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-04-12 17:30:54 +0200

    welcome

---
+++ Section1_content/welcome.php
@@ -0,0 +1,19 @@
+////// Object Oriented PHP & MVC Project without framworks //////
+
+// This is NOT a beginner based PHP project
+// We will be building a custom MVC framework from the ground up 
+// Once the framework is complete, we will build an app on top of it.
+// There will be an OPP primer section before starting the project.
+
+// Completely open source, can it what you want.
+// Will include full MVC workflow
+// Will use .htaccess and mod_rewrite for friendly URLs
+// Will have a base controller to load models & views
+// Will have a database library class that uses PDO 
+
+// Post Sharing Social App built on the custom framework
+// Will include full user authentication & access control
+// Will include full CRUD functionality for post 
+// Will include extra helpers for things like redirecting and flash messaging
+// We will deploy our app to shared hosting at the end.
+// We will start with creating the framework itself and move to creating an app with the framework.
---
+++ Section1_content/xampp_apache_setup.php
@@ -0,0 +1,17 @@
+//1 the first step is going to appache friends site and install it. 
+-> https://www.apachefriends.org/index.html
+   Apache Friends is a non-profit project to promote the Apache web server and is home to the XAMPP project. XAMPP is an easy to install Apache distribution containing MariaDB, PHP, and Perl.
+2. In the setup step 'Completing the XAMPP setup Wizard' we need to unchecked checkbox 'Do you want to start the Control Panel now?' and press the button finish. It will be install.
+3. Go to windows searh type and typing 'XAMPP Control panel'.
+4. Press right click on the Xampp Control panel and press 'Run Admin..'
+5. Press on the apache checkbox and mysql and install them.
+6. If you go in localhost, we can see dashboard.
+7. and we should be test that.
+   go to: Windows(C:) -> xampp -> htdocs and create test folder with index page (test -> index.php)
+
+8. Run phpmyadmin -> localhost/phpmyadmin/
+9. if we want to change password from database we can do it. 
+ go to User account -> database and change password. When we've done it, and if we want to open Database, it won't be able to do. we lost access for it. don't wory.
+ 10. go to WindowsC -> xampp -> phpMyAdmin -> config.inc.php and put a new password. save it and return back on the phpadmin page and reload it again. it should be work.
+ 11 Befor to start i'll recommend install extension in VSCode for PHP. It will be very hellpfully when we start coding.
+ -> PHP IntelliSense 
