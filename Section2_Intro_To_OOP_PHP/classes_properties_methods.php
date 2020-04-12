bd1d5eaef8460a3a530eb8e319f735e4df71a862
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-04-12 23:28:15 +0200

    add properties,methods and objects

--- Section2_Intro_To_OOP_PHP/classes_properties_methods.php
+++ Section2_Intro_To_OOP_PHP/classes_properties_methods.php
@@ -1,1 +1,27 @@
-<?php echo 'test';
+<?php 
+
+// Define a class
+class User {
+    // Properties(atributes)
+    //public $name = 'Milos';
+    public $name;
+
+    // Methods (functions)
+    public function sayHello() {
+        return $this->name . 'Says Hello';
+    }
+}
+
+// Instatiaste a user object fro, user class
+$user1 = new User();
+$user1->name = 'Milos';
+
+echo $user1->name;
+echo $user1->sayHello();
+
+// Create new user
+$user2 = new User();
+$user2->name = 'Miso';
+
+echo $user2->name;
+echo $user2->sayHello();
