6028f2cc53ff39ce649f240e50980206b1f8293c
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-04-14 17:18:06 +0200

    add configuration in config file and add uplouder

---
+++ Section3_Project/Tanaskoweb_MVC_Project/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+// App Root
+define('APPROOT', dirname(dirname(__FILE__))); 
+
+// URL Root
+define('URLROOT', 'http://localhost/PHP/oop_php_mvc_advance/Section3_Project/Tanaskoweb_MVC_Project');
+
+// Site Name
+
+define('SITENAME', 'Tanaskoweb_MVC');
