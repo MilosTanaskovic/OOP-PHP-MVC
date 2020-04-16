0df35363a7a47e6cdd67661dc994a5a2b7ca18a1
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-04-16 13:20:01 +0200

    create user model and email check...

--- Section3_Project/Tanaskoweb_MVC_Project/app/controller/Users.php
+++ Section3_Project/Tanaskoweb_MVC_Project/app/controller/Users.php
@@ -1,7 +1,7 @@
 <?php 
 class Users extends Controller {
     public function __construct(){
-
+        $this->userModel = $this->model('User');
     }
 
     public function register(){
@@ -31,6 +31,11 @@ class Users extends Controller {
             // Validate Email
             if(empty($data['email'])){
                 $data['email_err'] = 'Please enter email';
+            }else {
+                // Check email
+                if($this->userModel->findUserByEmail($data['email'])){
+                    $data['email_err'] = 'Email is already taken';
+                }
             }
             // Validate Password
             if(empty($data['password'])){
