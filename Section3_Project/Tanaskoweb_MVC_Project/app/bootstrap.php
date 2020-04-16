045d50fa0cda0459836e45167dd4ac1cbff82b25
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-04-16 14:14:12 +0200

    user registration...

--- Section3_Project/Tanaskoweb_MVC_Project/app/bootstrap.php
+++ Section3_Project/Tanaskoweb_MVC_Project/app/bootstrap.php
@@ -8,6 +8,8 @@ require_once 'config/config.php';
 // require_once 'libraries/Controller.php';
 // require_once 'libraries/Database.php';
 
+// Load helpers
+require_once 'helpers/url_helper.php';
 // Autoload Core Libraries
 spl_autoload_register(function($className){
     require_once 'libraries/' . $className . '.php';
