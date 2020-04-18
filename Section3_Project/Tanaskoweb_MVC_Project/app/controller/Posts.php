7b71441abc81b388c56d480d39c64576f72d9ff0
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-04-18 20:10:51 +0200

    editing post...

--- Section3_Project/Tanaskoweb_MVC_Project/app/controller/Posts.php
+++ Section3_Project/Tanaskoweb_MVC_Project/app/controller/Posts.php
@@ -79,4 +79,59 @@ class Posts extends Controller {
         ];
         $this->view('posts/show', $data);
     }
+
+    public function edit($id) {
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Sanitaze POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'id' => $id,
+                'user_id' => $_SESSION['user_id'],
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+            // Validate title
+            if(empty($data['title'])){
+                $data['title_err'] = 'Please enter title';
+            }
+            if(empty($data['body'])){
+                $data['body_err'] = 'Please enter body text';
+            }
+
+            // Make sure no errors
+            if(empty($data['title_err']) && empty($data['body_err'])){
+                // Validated
+                if($this->postModel->updatePost($data)){
+                    flash('post_message', 'Post Updated');
+                    redirect('posts');
+                } else {
+                    die('wrong');
+                }
+                
+            }else {
+                // Load view with error
+                $this->view('posts/edit', $data);
+            }
+        }else {
+            //Get existing post for model
+            $post = $this->postModel->getPostById($id);
+        // Check for owner
+        if($post->user_id != $_SESSION['user_id']){
+            redirect('posts');
+        }
+
+        $data = [
+            'id' => $id,
+            'title' => $post->title,
+            'body' => $post->body
+        ];
+
+        $this->view('posts/edit', $data);
+        }
+    }
 }
